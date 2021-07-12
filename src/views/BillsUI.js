3539c268fa9c26ccb3549ae8858a29fc35b2bd50
Goulven Nev <nevilain@gmail.com>
2021-07-12 10:41:10 +0200

    fix sort Bills

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -20,7 +20,8 @@ const row = (bill) => {
   }
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+
+  return (data && data.length) ? data.sort((a,b)=>{return new Date(b.date) - new Date(a.date)}).map(bill => row(bill)).join("") : ""
 }
 
 export default ({ data: bills, loading, error }) => {
