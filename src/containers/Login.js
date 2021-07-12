fd24c5f409a8feada87d6f80be9e0218df03aa37
Goulven Nev <nevilain@gmail.com>
2021-07-12 10:33:10 +0200

    fix admin login

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -35,8 +35,8 @@ export default class Login {
   handleSubmitAdmin = e => {
     const user = {
       type: "Admin",
-      email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
-      password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
+      email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
+      password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
       status: "connected"
     }
     this.localStorage.setItem("user", JSON.stringify(user))
