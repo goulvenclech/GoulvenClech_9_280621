727bcb4b6437e0de152df6c57991d0e1027f4ce3
Goulven Nev <nevilain@gmail.com>
2021-07-22 15:39:06 +0200

    Test GET Bills

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,9 +1,9 @@
 import {screen} from "@testing-library/dom"
-import userEvent from "@testing-library/user-event"
 import {setLocalStorage} from "../../setup-jest"
 import { bills } from "../fixtures/bills.js"
 import Bills from "../containers/Bills"
 import BillsUI from "../views/BillsUI.js"
+import firebase from "../__mocks__/firebase"
 
 // Setup
 const onNavigate = () => {return}
@@ -56,6 +56,35 @@ describe("Given I am connected as an employee", () => {
         sampleBills.handleClickIconEye(iconEye)
         expect($.fn.modal).toBeCalled()
         expect(document.querySelector(".modal")).toBeTruthy()
+      })
+    })
+  })
+  // test d'intégration GET
+  describe("Given I am a user connected as Employee", () => {
+    describe("When I navigate to Dashboard", () => {
+      test("fetches bills from mock API GET", async () => {
+        const getSpy = jest.spyOn(firebase, "get")
+        const bills = await firebase.get()
+        expect(getSpy).toHaveBeenCalledTimes(1)
+        expect(bills.data.length).toBe(4)
+      })
+      test("fetches bills from an API and fails with 404 message error", async () => {
+        firebase.get.mockImplementationOnce(() =>
+          Promise.reject(new Error("Erreur 404"))
+        )
+        const html = BillsUI({ error: "Erreur 404" })
+        document.body.innerHTML = html
+        const message = await screen.getByText(/Erreur 404/)
+        expect(message).toBeTruthy()
+      })
+      test("fetches messages from an API and fails with 500 message error", async () => {
+        firebase.get.mockImplementationOnce(() =>
+          Promise.reject(new Error("Erreur 500"))
+        )
+        const html = BillsUI({ error: "Erreur 500" })
+        document.body.innerHTML = html
+        const message = await screen.getByText(/Erreur 500/)
+        expect(message).toBeTruthy()
       })
     })
   })
